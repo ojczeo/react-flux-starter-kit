4c455f522f18994b0f9f28930aa43f117901987d
Andrzej <codelaborer@gmail.com>
2016-08-29 17:16:27 +0200

    Added notification

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,8 @@ var config = {
 		js: './src/**/*.js',
 		css: [
       		'node_modules/bootstrap/dist/css/bootstrap.min.css',
-      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+					'node_modules/toastr/toastr.scss'
     	],
 		dist: './dist',
 		mainJs: './src/main.js'
--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,6 +4,7 @@ var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
 
@@ -28,6 +29,7 @@ var ManageAuthorPage = React.createClass({
   saveAuthor: function(e) {
     e.preventDefault();
     AuthorApi.saveAuthor(this.state.author);
+    toastr.success('Author saved.');
     this.transitionTo('authors');
   },
 
