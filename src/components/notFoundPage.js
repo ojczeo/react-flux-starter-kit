89e3cd158baacb967915f874dfb0da8068bd18da
Andrzej <codelaborer@gmail.com>
2016-08-29 15:24:39 +0200

    Transition routes for router added

--- src/components/notFoundPage.js
+++ src/components/notFoundPage.js
@@ -5,13 +5,13 @@ var Link = require("react-router").Link;
 
 var NotFoundPage = React.createClass({
   render: function() {
-    return(
+    return (
       <div>
         <h1>Page Not Found</h1>
         <p>Whoops! Sorry, there is nothing to see here.</p>
         <p><Link to="app">Back to home</Link></p>
       </div>
-    )
+    );
   }
 });
 
