851eaf1a29ed655eef98b6acb6a17320234ea5e5
Andrzej <codelaborer@gmail.com>
2016-08-29 12:38:26 +0200

    Initial case

---
+++ src/components/homepage.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var React = require('react');
+
+var Homepage = React.createClass({
+  render: function() {
+
+    return (
+      <div className="jumbotron">
+        <h1>Administration</h1>
+        <p>React, React Router Flux</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Homepage;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,39 @@
+"use strict";
+
 $ = jQuery = require('jquery');
+var React = require('react');
+var Homepage = require('./components/homepage');
+var About = require('./components/about/aboutPage');
 
-var App = console.log('Hello world from Browserify');
+(function(win) {
 
-module.exports = App;
+  var App = React.createClass({
+    render: function() {
+      var Child;
+
+      switch(this.props.route) {
+        case 'about': Child = About; break;
+        default: Child = Homepage;
+      }
+
+      return (
+        <div>
+          <Child />
+        </div>
+      );
+    }
+  });
+
+  function render() {
+    var route = window.location.hash.substr(1);
+    React.render(<App route={route} />, document.getElementById('app'));
+  }
+
+  win.addEventListener("hasChange", render);
+  render();
+
+  React.render(<Homepage />, document.getElementById('app'));
+})(window);
+
+
+// module.exports = App;
